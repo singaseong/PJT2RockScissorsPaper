f5de5e63df3a42422b52f0a0ce789a043d2f3a71
singaseong <seonghwanlee@gmail.com>
2024-03-15 15:26:42 +0900

    refactoring, adding funtions

--- app/src/main/java/com/example/pjt2rockscissorspaper/MainActivity.kt
+++ app/src/main/java/com/example/pjt2rockscissorspaper/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,11 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,18 +26,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pjt2rockscissorspaper.ui.theme.PJT2RockScissorsPaperTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,6 +71,7 @@ fun PJT2RockScissorsPaper() {
     var winCountComputer by remember {mutableStateOf(0)}
     var totalGameCount by remember {mutableStateOf(0)}
 
+    //function to randomly choose computer's choice
     fun comChoice() {
         val randomNumber = (1..3).random()
 
@@ -102,6 +93,7 @@ fun PJT2RockScissorsPaper() {
         }
     }
 
+    //function to determine winner and update win counts
     fun winnerCheck () {
         winner = when {
             playerChoice == computerChoice -> "Tie"
@@ -118,10 +110,9 @@ fun PJT2RockScissorsPaper() {
         } else {
 
         }
-
-
     }
 
+    //Composable function to display player's option choice image.
     @Composable
     fun playerChoiceImage (id:Int, choice:String) {
         Image(
@@ -132,11 +123,12 @@ fun PJT2RockScissorsPaper() {
                 .clickable(
                     onClick = {
                         playerChoice = choice
-                        winner =""
+                        winner = ""
                     })
         )
     }
 
+    //Composable function to display selected choice image with size changeable
     @Composable
     fun selectedImage(id:Int, size:Dp){
         Image(
@@ -146,7 +138,60 @@ fun PJT2RockScissorsPaper() {
                 .size(size)
         )
     }
-    
+    //Composable function to display player's result & win count
+    @Composable
+    fun showPlayer() {
+
+            Text(text = "Player",fontSize = 24.sp)
+            Text(text = "${playerChoice}",fontSize = 18.sp)
+            selectedImage(chosenImagePlayer,48.dp)
+            Text(text = "Win : ${winCountPlayer}",fontSize = 18.sp)
+
+    }
+    //Composable function to display Computer's result & win count
+    @Composable
+    fun showComputer(){
+            Text(text = "Computer",fontSize = 24.sp)
+            Text(text = "${computerChoice}",fontSize = 18.sp)
+            selectedImage(chosenImageComputer,48.dp)
+            Text(text = "Win : ${winCountComputer}",fontSize = 18.sp)
+
+    }
+
+    //Composable function to display player vs computer result section
+    @Composable
+    fun showPlayerVSComputer(){
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(2.5f)
+            ) {
+                showPlayer()
+            }
+
+            Text(
+                text = "VS\nTie\n${totalGameCount - winCountComputer - winCountPlayer}",
+                fontSize = 18.sp,
+                modifier = Modifier.weight(1.2f),
+                textAlign = TextAlign.Center
+            )
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.weight(2.5f)
+            ) {
+                showComputer()
+            }
+
+
+        }
+    }
+
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -214,7 +259,7 @@ fun PJT2RockScissorsPaper() {
                 onClick = {
                     comChoice()
                     winnerCheck ()
-                    totalGameCount += 1
+                    totalGameCount++
                 }
 
             ){
@@ -229,82 +274,16 @@ fun PJT2RockScissorsPaper() {
         if(winner !="" && winner !="Tie") {
             Text(text = "Total Game : ${totalGameCount}ea",fontSize = 28.sp)
             Text(text = "Winner : ${winner}",fontSize = 28.sp)
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
-            ){
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.weight(2.5f)
-                ) {
-                    Text(text = "Player",fontSize = 24.sp)
-                    Text(text = "${playerChoice}",fontSize = 18.sp)
-                    selectedImage(chosenImagePlayer,48.dp)
-                    Text(text = "Win : ${winCountPlayer}",fontSize = 18.sp)
-                }
-                Text(
-                    text = "VS",
-                    fontSize = 24.sp,
-                    modifier = Modifier.weight(1f),
-                    textAlign = TextAlign.Center
-                )
-
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.weight(2.5f)
-                ) {
-                    Text(text = "Computer",fontSize = 24.sp)
-                    Text(text = "${computerChoice}",fontSize = 18.sp)
-                    selectedImage(chosenImageComputer,48.dp)
-                    Text(text = "Win : ${winCountComputer}",fontSize = 18.sp)
-
-                }
-
-            }
-
+            showPlayerVSComputer()
 
         }else if(winner =="Tie") {
             Text(text = "Total Game : ${totalGameCount}ea",fontSize = 28.sp)
-            Text(text = "Draw",fontSize = 28.sp)
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
-            ){
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.weight(2.5f)
-                ) {
-                    Text(text = "Player",fontSize = 24.sp)
-                    Text(text = "${playerChoice}",fontSize = 18.sp)
-                    selectedImage(chosenImagePlayer,48.dp)
-                    Text(text = "Win : ${winCountPlayer}",fontSize = 18.sp)
-                }
-                Text(
-                    text = "VS",
-                    fontSize = 24.sp,
-                    modifier = Modifier.weight(1f),
-                    textAlign = TextAlign.Center
-                )
+            Text(text = "Tie",fontSize = 28.sp)
+            showPlayerVSComputer()
 
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.weight(2.5f)
-                ) {
-                    Text(text = "Computer",fontSize = 24.sp)
-                    Text(text = "${computerChoice}",fontSize = 18.sp)
-                    selectedImage(chosenImageComputer,48.dp)
-                    Text(text = "Win : ${winCountComputer}",fontSize = 18.sp)
-
-                }
-
-            }
         }else {
             //nothing
         }
-        //Spacer(modifier = Modifier.height(16.dp))
 
         //RESET
         Spacer(modifier = Modifier.weight(1f))
